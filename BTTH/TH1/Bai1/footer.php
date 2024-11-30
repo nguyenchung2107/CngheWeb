2cd94cbbbfef370e785e1bacc858372ef7a080d2
nguyenchung2107 <nguyenbaochung2107@gmail.com>
2024-11-30 11:49:48 +0700

    Delete BTTH/TH1/Bai1 directory

--- BTTH/TH1/Bai1/footer.php
+++
@@ -1,6 +0,0 @@
-<footer class="text-center py-3 mt-4 border-top">
-<p style="font-weight: bold; font-size: 25px; text-align: center; margin-top: 40px;"> © 2024 Các loại hoa phù hợp với bạn.</p>
-</footer>
-    <script src="./bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js"></script>
-</body>
-</html>
